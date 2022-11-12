1014a026fe465f6d3fddf384e817b1a02ab2e1a3
Leandro Rondon <lerondon@gmail.com>
2022-11-12 13:45:32 +0000

    Move migrations to a 'bronze' database

--- cmd/migrate/migrate.go
+++ cmd/migrate/migrate.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"log"
 
 	"github.com/golang-migrate/migrate/v4"
@@ -8,17 +10,42 @@ import (
 	_ "github.com/golang-migrate/migrate/v4/source/file"
 )
 
+type logger struct {
+	*log.Logger
+}
+
+func (l *logger) Verbose() bool {
+	return false
+}
+
 func main() {
+	migrateDB("bronze")
+	// TODO: Create silver and gold databases
+}
+
+func migrateDB(name string) {
+	log.Println("Migrating database", name)
+
 	m, err := migrate.New(
-		"file://./migrations",
-		"postgres://postgres:postgres@localhost:5432/eleicoes?sslmode=disable")
+		fmt.Sprintf("file://./migrations/%s", name),
+		fmt.Sprintf("postgres://postgres:postgres@db.local:5432/%s?sslmode=disable", name))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	err = m.Up() // or m.Step(2) if you want to explicitly set the number of migrations to run
-	if err != nil {
+	m.Log = &logger{
+		Logger: log.Default(),
+	}
+
+	err = m.Up()
+	if err != nil && !errors.Is(err, migrate.ErrNoChange) {
 		log.Fatal(err)
 	}
 
+	if err != nil && errors.Is(err, migrate.ErrNoChange) {
+		log.Println("No change.")
+		return
+	}
+
+	log.Println("Done.")
 }
