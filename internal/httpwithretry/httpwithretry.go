958a3c66e22095494b3eba09da7ae5cee36ed917
Leandro Rondon <lerondon@gmail.com>
2022-11-16 15:00:46 +0000

    Delay between http retries.

--- internal/httpwithretry/httpwithretry.go
+++ internal/httpwithretry/httpwithretry.go
@@ -2,11 +2,13 @@ package httpwithretry
 
 import (
 	"net/http"
+	"time"
 )
 
 func Get(url string, retry int) (*http.Response, error) {
 	resp, err := http.Get(url)
 	if err != nil && retry > 0 {
+		time.Sleep(3 * time.Second)
 		return Get(url, retry-1)
 	}
 
